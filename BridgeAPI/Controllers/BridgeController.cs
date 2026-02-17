7168a7c501d9286e0d76755264605c83e7f64584
danielkurtsawyer <[19danielsawyer@gmail.com]>
2026-02-17 12:42:40 -0500

    Update Controller dependency injection with bridge_service

--- BridgeAPI/Controllers/BridgeController.cs
+++ BridgeAPI/Controllers/BridgeController.cs
@@ -4,30 +4,30 @@ using Microsoft.AspNetCore.Mvc;
 [Route("api/[controller]")]
 public class BridgeController : ControllerBase
 {
-  private readonly IBridgeRepository _bridgeRepository;
-  public BridgeController(IBridgeRepository repository)
+  private readonly IBridgeService _bridgeService;
+  public BridgeController(IBridgeService service)
   {
-    _bridgeRepository = repository;
+    _bridgeService = service;
   }
 
   [HttpGet]
   public IActionResult GetBridges()
   {
-    return Ok(_bridgeRepository.GetBridges());
+    return Ok(_bridgeService.GetBridges());
   }
 
   [HttpGet("{id}")]
   [ServiceFilter(typeof(Bridge_ValidateBridgeIdFilterAttribute))]
   public IActionResult GetBridge(int id)
   {
-    return Ok(_bridgeRepository.GetBridgeById(id));
+    return Ok(_bridgeService.GetBridgeById(id));
   }
 
   [HttpPost]
   [ServiceFilter(typeof(Bridge_ValidateBridgeCreateFilterAttribute))]
   public IActionResult CreateBridge(Bridge bridge)
   {
-    _bridgeRepository.AddBridge(bridge);
+    _bridgeService.AddBridge(bridge);
     return CreatedAtAction(nameof(GetBridge), new { id = bridge.BridgeId }, bridge);
   }
 
@@ -36,7 +36,7 @@ public class BridgeController : ControllerBase
   [Bridge_ValidateBridgeUpdateFilter]
   public IActionResult UpdateBridge(int id, Bridge bridge)
   {
-    _bridgeRepository.UpdateBridge(bridge);
+    _bridgeService.UpdateBridge(bridge);
     return NoContent();
   }
 
@@ -44,8 +44,8 @@ public class BridgeController : ControllerBase
   [ServiceFilter(typeof(Bridge_ValidateBridgeIdFilterAttribute))]
   public IActionResult DeleteBridge(int id)
   {
-    var bridge = _bridgeRepository.GetBridgeById(id);
-    _bridgeRepository.DeleteBridge(id);
+    var bridge = _bridgeService.GetBridgeById(id);
+    _bridgeService.DeleteBridge(id);
     return Ok(bridge);
   }
 }
