8498c207807afc2e1b14dc543ad2e120159eaa63
danielkurtsawyer <[19danielsawyer@gmail.com]>
2026-02-17 12:52:31 -0500

    Register BridgeService for Dependency Injection

--- BridgeAPI/Program.cs
+++ BridgeAPI/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddSingleton<IBridgeRepository, BridgeRepository>();
+builder.Services.AddScoped<IBridgeService, BridgeService>();
 builder.Services.AddScoped<Bridge_ValidateBridgeCreateFilterAttribute>();
 builder.Services.AddScoped<Bridge_ValidateBridgeIdFilterAttribute>();
 
