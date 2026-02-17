bc36a0e892a8a189cc8bda31ac30a1a5f08e20a4
danielkurtsawyer <[19danielsawyer@gmail.com]>
2026-02-17 12:23:52 -0500

    Register filters with dependencies as servicefilters

--- BridgeAPI/Program.cs
+++ BridgeAPI/Program.cs
@@ -3,6 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddSingleton<IBridgeRepository, BridgeRepository>();
+builder.Services.AddScoped<Bridge_ValidateBridgeCreateFilterAttribute>();
+builder.Services.AddScoped<Bridge_ValidateBridgeIdFilterAttribute>();
 
 var app = builder.Build();
 
